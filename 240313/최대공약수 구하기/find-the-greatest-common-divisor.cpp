2054c687dafd09e94bf39bdd67932ad5f3fc75e8
PT-Jun0915 <127864579+PT-Jun0915@users.noreply.github.com>
2024-03-13 00:38:05 +0900

    Title: 최대공약수 구하기, Time: 14ms, Memory: 0MB, Status: Runtime Error - CodeTree

---
+++ 240313/최대공약수 구하기/find-the-greatest-common-divisor.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+
+int gcd(int n, int m)
+{
+	int min{ (m > n ? n : m) };
+	while (min--)
+	{
+		if (m % min == 0 && n % min == 0)
+		{
+			return min;
+		}
+	}
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);	cout.tie(NULL);
+
+	int n, m;
+	cin >> n >> m;
+	cout << gcd(n, m);
+}
