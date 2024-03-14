1e8204eb22eae1f76b4abb831ad8e1e1aa6494cb
PT-Jun0915 <127864579+PT-Jun0915@users.noreply.github.com>
2024-03-14 12:28:41 +0900

    Title: 함수를 이용한 369 게임, Time: 8ms, Memory: 0MB, Status: Wrong Answer - CodeTree

---
+++ 240314/함수를 이용한 369 게임/369-games-using-functions.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+bool Is_true(int n)
+{
+	while (n > 0)
+	{
+		if (n % 10 % 3 == 0 || n % 3 == 0) return true;
+		n /= 10;
+	}
+	return false;
+}
+
+int count(int a, int b)
+{
+	int ans{ 0 };
+	for (int i{ a }; i <= b; ++i)
+	{
+		if (Is_true(i)) ++ans;
+	}
+	return ans;
+}
+
+int main()
+{
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);	cout.tie(NULL);
+
+	int a, b;
+	cin >> a >> b;
+	cout << count(a, b);
+}
